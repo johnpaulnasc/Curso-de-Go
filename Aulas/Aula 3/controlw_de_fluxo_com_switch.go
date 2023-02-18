828dedddbf6a3caa49893e8b675b036493c581e3
João Borges <joaopauloborges1995@gmail.com>
2023-02-18 19:46:05 -0300

    up

--- Aulas/Aula 3/controlw_de_fluxo_com_switch.go
+++ Aulas/Aula 3/controlw_de_fluxo_com_switch.go
@@ -3,12 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	nome := "João"
-	idade := 27
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
-	fmt.Println("Este programa está na versão", versao)
-
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
---
+++ Aulas/Aula 4/iniciando_monitoramento.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for {
+		exibeMenu()
+
+		comando := lerComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+	}
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func lerComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://v4h.cloud/"
+	resp, _ := http.Get(site)
+	//fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
+}
