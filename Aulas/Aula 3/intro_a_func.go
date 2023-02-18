0daa8ebc839284165dd89662817a404f000bf1bf
João Borges <joaopauloborges1995@gmail.com>
2023-02-18 19:16:59 -0300

    up

---
+++ Aulas/Aula 3/intro_a_func.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := lerComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
+	nome := "João"
+	idade := 27
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func lerComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
